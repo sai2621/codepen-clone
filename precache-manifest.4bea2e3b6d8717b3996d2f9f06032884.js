d7e9b965bc36eeb1da23fcf7cd9fd024cd05bd0c
sai250 <dsaibhargav567@gmail.com>
2020-10-30 20:01:35 +0530

    Updates

---
+++ precache-manifest.4bea2e3b6d8717b3996d2f9f06032884.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b89ef33643ed82a7fff6409b4819f58",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "c80ee48ca9dd91d18ccc",
+    "url": "/codepen-clone/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen-clone/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-clone/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80ee48ca9dd91d18ccc",
+    "url": "/codepen-clone/static/js/main.8f179c20.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
